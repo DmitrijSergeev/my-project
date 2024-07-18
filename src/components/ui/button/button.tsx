daf6c4bc371267aabf6fd071d4940f86ae3f7344
Dmitry Sergeev <dmitrijsergeev@gmail.com>
2024-07-18 21:04:23 +0400

    button

---
+++ src/components/ui/button/button.tsx
@@ -0,0 +1,9 @@
+import { ComponentPropsWithoutRef } from 'react'
+
+import s from './button.module.scss'
+
+export type ButtonProps = ComponentPropsWithoutRef<'button'>
+
+export const Button = () => {
+  return <div></div>
+}
