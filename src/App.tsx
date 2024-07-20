e9036163b6a911ecec780bb1330eb9d2cb57a9ec
Dmitry Sergeev <dmitrijsergeev@gmail.com>
2024-07-20 13:07:53 +0400

    typography

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,65 @@
+import { useState } from 'react'
+
+export type TimeIntervalItemType = {
+  end: string
+  is_working: boolean
+  next_day_end: boolean
+  start: string
+}
+export type TimeIntervals = {
+  [key: string]: TimeIntervalItemType
+}
+
+const weeklySchedule = {
+  '0': {
+    end: '23:59',
+    is_working: false,
+    next_day_end: true,
+    start: '00:00',
+  },
+  '1': {
+    end: '23:59',
+    is_working: false,
+    next_day_end: true,
+    start: '00:00',
+  },
+  '2': {
+    end: '23:59',
+    is_working: false,
+    next_day_end: true,
+    start: '00:00',
+  },
+  '3': {
+    end: '23:59',
+    is_working: false,
+    next_day_end: true,
+    start: '00:00',
+  },
+  '4': {
+    end: '23:59',
+    is_working: false,
+    next_day_end: true,
+    start: '00:00',
+  },
+  '5': {
+    end: '23:59',
+    is_working: false,
+    next_day_end: true,
+    start: '00:00',
+  },
+  '6': {
+    end: '23:59',
+    is_working: false,
+    next_day_end: true,
+    start: '00:00',
+  },
+} as TimeIntervals
+
 export function App() {
+  const [timeIntervals, setTimeIntervals] = useState<TimeIntervals>(weeklySchedule)
+  const updateTimeInterval = (time: TimeIntervals) => {
+    setTimeIntervals(time)
+  }
+
   return <div>Hello</div>
 }
--- src/components/ui/button/button.tsx
+++
@@ -1,9 +0,0 @@
-import { ComponentPropsWithoutRef } from 'react'
-
-import s from './button.module.scss'
-
-export type ButtonProps = ComponentPropsWithoutRef<'button'>
-
-export const Button = () => {
-  return <div></div>
-}
