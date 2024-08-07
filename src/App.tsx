6c586c758663563611b7d2f66afcf32b878fea46
Dmitry Sergeev <dmitrijsergeev@gmail.com>
2024-08-07 20:26:12 +0400

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,9 @@
-import { useState } from 'react'
-
-import { Block } from './components/block'
-import { WeeklySchedule } from './components/weekelySchedule'
-
-export type TimeIntervalItemType = {
-  end: string
-  is_working: boolean
-  next_day_end: boolean
-  start: string
-}
-export type TimeIntervals = {
-  [key: string]: TimeIntervalItemType
-}
-
-const weeklySchedule = {
-  '0': {
-    end: '23:59',
-    is_working: false,
-    next_day_end: true,
-    start: '00:00',
-  },
-  '1': {
-    end: '23:59',
-    is_working: false,
-    next_day_end: true,
-    start: '00:00',
-  },
-  '2': {
-    end: '23:59',
-    is_working: false,
-    next_day_end: true,
-    start: '00:00',
-  },
-  '3': {
-    end: '23:59',
-    is_working: false,
-    next_day_end: true,
-    start: '00:00',
-  },
-  '4': {
-    end: '23:59',
-    is_working: false,
-    next_day_end: true,
-    start: '00:00',
-  },
-  '5': {
-    end: '23:59',
-    is_working: false,
-    next_day_end: true,
-    start: '00:00',
-  },
-  '6': {
-    end: '23:59',
-    is_working: false,
-    next_day_end: true,
-    start: '00:00',
-  },
-} as TimeIntervals
+import { Table } from './components/Table'
 
 export function App() {
-  const [timeIntervals, setTimeIntervals] = useState<TimeIntervals>(weeklySchedule)
-
-  const updateTimeInterval = (time: TimeIntervals) => {
-    setTimeIntervals(time)
-  }
-
   return (
     <>
-      <Block title={'Расписание'}>
-        <WeeklySchedule timeIntervals={timeIntervals} updateTimeIntervals={updateTimeInterval} />
-      </Block>
+      <Table />
     </>
   )
 }
---
+++ src/components/Table.tsx
@@ -0,0 +1,15 @@
+const useMockrequest = () => {
+  // const request = ({
+  //     return new Promise((res, rej)=>{
+  //         setTimeout(()=>{
+  //
+  //         })
+  //     })
+  // })
+}
+
+export const Table = () => {
+  const request = useMockrequest()
+
+  return <div></div>
+}
